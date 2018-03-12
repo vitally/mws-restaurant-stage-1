ba25836f402d9f08f3b9a106bcda2ee6f87b853a
Vitally Tezhe <vitally.tezhe@gmail.com>
2018-03-12 15:43:43 +0200

    fix: adding service worke to restaurant_info.js

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,6 +1,16 @@
 let restaurant;
 var map;
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/sw.js').then(() => {
+      console.log('Service Worker Registerd');
+    }).catch(() => {
+      console.error('Service Worker registration failed');
+    });
+  });
+}
+
 /**
  * Initialize Google map, called from HTML.
  */
